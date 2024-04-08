81122d7c5191bf3c63f488fa8be274d450b5a479
huntj0n <hunt.jon19@gmail.com>
2024-04-08 17:31:01 -0600

    error handling

--- cards/main.go
+++ cards/main.go
@@ -1,19 +1,10 @@
-// // savinng data to the hard drive (run file in terminal before pushing)
-// // reading from the hard drive
+// error handling
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_cards")
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
-
-// error handling
-// package main
-
-// func main() {
-// 	// cards := newDeck()
-// 	// cards.saveToFile("my_cards")
-
-// 	cards := newDeckFromFile("my_cards")
-// 	cards.print()
-// }
