b9558b8ac40d5912d146033c22f231ac61221563
huntj0n <hunt.jon19@gmail.com>
2024-04-08 17:25:30 -0600

    creating a new deck

--- cards/main.go
+++ cards/main.go
@@ -1,3 +1,4 @@
+// creating a new deck
 package main
 
 func main() {
@@ -5,3 +6,45 @@ func main() {
 
 	cards.print()
 }
+
+// // multiple return values
+// package main
+
+// func main() {
+// 	cards := newDeck()
+
+// 	hand, remainingCards := deal(cards, 5)
+
+// 	hand.print()
+// 	remainingCards.print()
+// }
+
+// // byte slices, deck to string, joining a slice of strings
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	cards := newDeck()
+// 	fmt.Println(cards.toString())
+// }
+
+// // savinng data to the hard drive (run file in terminal before pushing)
+// // reading from the hard drive
+// package main
+
+// func main() {
+// 	cards := newDeck()
+// 	cards.saveToFile("my_cards")
+// }
+
+// error handling
+// package main
+
+// func main() {
+// 	// cards := newDeck()
+// 	// cards.saveToFile("my_cards")
+
+// 	cards := newDeckFromFile("my_cards")
+// 	cards.print()
+// }
