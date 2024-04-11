b175cc021550f0efd197db7211e71757900c14cb
huntj0n <hunt.jon19@gmail.com>
2024-04-11 13:41:31 -0600

    shuffling a deck

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,13 @@
-// error handling
 package main
 
 func main() {
 	// cards := newDeck()
 	// cards.saveToFile("my_cards")
 
-	cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
