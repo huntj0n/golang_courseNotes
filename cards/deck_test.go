0b92f8cc99fd810ddaec9d70f73f02789775a117
huntj0n <hunt.jon19@gmail.com>
2024-04-17 21:20:13 -0600

    writing useful tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,2 +1,13 @@
 package main
 
+import "testing"
+
+// t.Errorf("Expected deck length of 16, but got %v", len(d))
+
+func TestNewDeck(t *testing.T){
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+}
